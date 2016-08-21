417fcfc1882f097c3bcb775903909c9bf2e5febe
Paul Mucur <mudge@mudge.name>
2016-08-21 15:13:03 +0100

    Correct block lengths in specs

--- spec/oracle_spec.rb
+++ spec/oracle_spec.rb
@@ -6,33 +6,33 @@ RSpec.describe Oracle do
     it 'decrypts DES ciphertexts with a length that is a multiple of block size' do
       cipher = Cipher.new('DES-CBC')
       oracle = Oracle.new(cipher)
-      ciphertext = cipher.encrypt('123456781234567812345678')
+      ciphertext = cipher.encrypt('012345670123456701234567')
 
-      expect(oracle.attack(ciphertext)).to eq("1234567812345678\x8\x8\x8\x8\x8\x8\x8\x8")
+      expect(oracle.attack(ciphertext)).to eq("0123456701234567\x8\x8\x8\x8\x8\x8\x8\x8")
     end
 
     it 'decrypts DES ciphertexts with a length that is not a multiple of block size' do
       cipher = Cipher.new('DES-CBC')
       oracle = Oracle.new(cipher)
-      ciphertext = cipher.encrypt('12345678123456781234')
+      ciphertext = cipher.encrypt('01234567012345670123')
 
-      expect(oracle.attack(ciphertext)).to eq("123456781234\x4\x4\x4\x4")
+      expect(oracle.attack(ciphertext)).to eq("012345670123\x4\x4\x4\x4")
     end
 
     it 'decrypts AES ciphertexts with a length that is a multiple of block size' do
       cipher = Cipher.new('AES-128-CBC')
       oracle = Oracle.new(cipher)
-      ciphertext = cipher.encrypt('123456781234567812345678')
+      ciphertext = cipher.encrypt('0123456789abcdef0123456789abcdef')
 
-      expect(oracle.attack(ciphertext)).to eq("12345678\x8\x8\x8\x8\x8\x8\x8\x8")
+      expect(oracle.attack(ciphertext)).to eq("0123456789abcdef\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10")
     end
 
     it 'decrypts AES ciphertexts with a length that is not a multiple of block size' do
       cipher = Cipher.new('AES-128-CBC')
       oracle = Oracle.new(cipher)
-      ciphertext = cipher.encrypt('12345678123456781234')
+      ciphertext = cipher.encrypt('0123456789abcdef0123456789abcdef01234567')
 
-      expect(oracle.attack(ciphertext)).to eq("1234\xc\xc\xc\xc\xc\xc\xc\xc\xc\xc\xc\xc")
+      expect(oracle.attack(ciphertext)).to eq("0123456789abcdef01234567\x8\x8\x8\x8\x8\x8\x8\x8")
     end
   end
 end
